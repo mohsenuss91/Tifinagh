5fb489cd5c59f12cb0eabc7e31a4d5ee3cebf01e
boudeffa <boudeffa@gmail.com>
2015-03-30 18:28:43 +0200

    Compiled with AndEngine but not working yet, i think to use Rajawali for comparison

---
+++ src/biz/aldaffah/tifinagh/games/AndEngineGame.java
@@ -0,0 +1,77 @@
+package biz.aldaffah.tifinagh.games;
+
+import org.andengine.engine.camera.Camera;
+import org.andengine.engine.options.EngineOptions;
+import org.andengine.engine.options.ScreenOrientation;
+import org.andengine.engine.options.resolutionpolicy.RatioResolutionPolicy;
+import org.andengine.entity.scene.Scene;
+import org.andengine.entity.scene.background.Background;
+import org.andengine.entity.sprite.Sprite;
+import org.andengine.opengl.texture.TextureOptions;
+import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlas;
+import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlasTextureRegionFactory;
+import org.andengine.opengl.texture.region.TextureRegion;
+import org.andengine.ui.activity.SimpleBaseGameActivity;
+import org.andengine.util.color.Color;
+
+public class Game extends SimpleBaseGameActivity {
+    // ===========================================================
+    // Constants
+    // ===========================================================
+    private static final int CAMERA_WIDTH = 320;
+    private static final int CAMERA_HEIGHT = 480;
+
+    private Camera    m_Camera;
+    private Scene    m_Scene;
+
+    // ===========================================================
+    // Fields
+    // ===========================================================
+    private BitmapTextureAtlas texImage;
+    private TextureRegion  regImage;
+    private Sprite   sprImage;
+
+    // ===========================================================
+    // Constructors
+    // ===========================================================
+
+    // ===========================================================
+    // Getter & Setter
+    // ===========================================================
+
+    // ===========================================================
+    // Methods for/from SuperClass/Interfaces
+    // ===========================================================
+
+    @Override
+    public EngineOptions onCreateEngineOptions()
+    {
+        m_Camera = new Camera(0, 0, CAMERA_WIDTH, CAMERA_HEIGHT);
+        EngineOptions en = new EngineOptions(true, ScreenOrientation.PORTRAIT_FIXED, new RatioResolutionPolicy(
+                CAMERA_WIDTH, CAMERA_HEIGHT), m_Camera);
+
+        return en;
+    }
+
+    @Override
+    protected void onCreateResources()
+    {
+        // TODO Auto-generated method stub
+        texImage = new BitmapTextureAtlas(this.getTextureManager(), 512, 512, TextureOptions.BILINEAR_PREMULTIPLYALPHA);
+        regImage = BitmapTextureAtlasTextureRegionFactory.createFromAsset(texImage, this.getAssets(), "gfx/bijoux-messages/a.jpg",0,0);
+        texImage.load();
+
+    }
+
+    @Override
+    protected Scene onCreateScene()
+    {
+        m_Scene = new Scene();
+        m_Scene.setBackground(new Background(Color.WHITE));
+
+        sprImage = new Sprite(0, 0, regImage, this.getVertexBufferObjectManager());
+        m_Scene.attachChild(sprImage);
+        return m_Scene;
+    }
+
+}
